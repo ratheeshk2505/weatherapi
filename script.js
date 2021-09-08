3cb74d5c738b56f0b69eca3a859353d380502866
Ratheesh K <ratheeshk2505@gmail.com>
2021-09-08 22:34:48 +0530

    minute correction

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var input = document.getElementById("city");
-  input.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-     event.preventDefault();
-     document.getElementById("myBtn").click();
-    }
-  });
+// var input = document.getElementById("city");
+//   input.addEventListener("keyup", function(event) {
+//     if (event.keyCode === 13) {
+//      event.preventDefault();
+//      document.getElementById("myBtn").click();
+//     }
+//   });
 
 
 function fetchTemp(){
