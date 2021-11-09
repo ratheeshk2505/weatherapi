456686fdb18ef2e514b421b3b1b2c314e8d697f5
Ratheesh K <ratheeshk2505@gmail.com>
2021-11-09 12:09:16 +0530

    alighnment and favicon

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function fetchTemp(){
 
 function displayValues(data){
         if(data.cod>399){
-            disp.innerHTML=`<div class="text-center text-white p-1 mt-3 border border-secondary col-12" style="background-color: rgb(0, 0, 0"><span class="fs-2">City Not Found.</span></div>`;
+            disp.innerHTML=`<div class="text-center text-white p-1 mt-3 border border-secondary rounded-3 col-12" style="background-color: rgb(0, 0, 0)"><span class="fs-2">City Not Found.</span></div>`;
         }
         else{
         let mytz=19800;
@@ -22,18 +22,18 @@ function displayValues(data){
         let loc_time=unixToDate(unixData);
         let icon_url=`https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
         let html_data=`<div class="container-fluid">
-                        <div class="row mt-3 border border-dark bg-light">
-                            <div class="text-center text-white p-0 " style="background-color: rgb(0, 0, 0"><span class="fs-1 fw-larger">${data.name}, ${data.sys.country}</span></div>
+                        <div class="row mt-3 border border-dark rounded-3 bg-light">
+                            <div class="text-center text-white p-0 " style="background-color: rgb(0, 0, 0)"><span class="fs-1 fw-larger">${data.name}, ${data.sys.country}</span></div>
                             <div class="text-end text-white" style="background-color: rgb(0, 0, 0"><span class="fs-6"> As on: ${loc_time}</span></div>
-                            <div class="col-sm-12 col-md-6 col-lg-4 p-3 mt-4 text-center"><img src="${icon_url}" width="100px"><span class="fs-2"> <b>${data.weather[0].main}</b></span><br><span class="ms-5 fs-6 fst-italic">  ${data.weather[0].description}</span></div>
-                            <div class="col-sm-12 col-md-6 col-lg-4 p-3 text-center">
+                            <div class="col-sm-12 col-md-12 col-lg-4 p-3 mt-4 text-center"><img src="${icon_url}" width="100px"><span class="fs-2"> <b>${data.weather[0].main}</b></span><br><span class="ms-5 fs-6 fst-italic">  ${data.weather[0].description}</span></div>
+                            <div class="col-sm-12 col-md-12 col-lg-4 p-3 text-center">
                                 <div class="p-2"><span class="" style="font-size: 50px"> <b>${data.main.temp}</b> <sup><span class="fs-5 fw-bold"> &#8451;</span> <span class="fs-5" style="color: grey">| &#8457;</span></sup></span></div>
                                 <div class="p-3 my-3">
                                     <i class="fas fa-temperature-high"></i><span class="fs-5"> Highest Temp: <b>${data.main.temp_max}</b></span><sup><span class=""> &#8451;</span></sup><br>
                                     <i class="fas fa-temperature-low"></i><span class="fs-5"> Lowest Temp: <b>${data.main.temp_min}</b></span><sup><span class=""> &#8451;</span></sup>
                                 </div>
                             </div>
-                            <div class="col-sm-12 col-md-6 col-lg-4 p-3">
+                            <div class="col-sm-12 col-md-12 col-lg-4 p-3">
                                 <div class="p-3 text-center">
                                     <span class="fs-5 p-2"> Humidity: <b>${data.main.humidity}</b></span><span>&#37;</span> <br>
                                     <span class="fs-5 p-2"> Pressure: <b>${data.main.pressure}</b></span><span>hPa</span>
